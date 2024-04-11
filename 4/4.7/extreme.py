941ba9ce641b30f6bfa8b9c7cc3aa18aafd7e9d5
Gritsaev Sergei <sergey_gritsaev@mail.ru>
2024-04-11 17:15:54 +0300

    Modified strext, added extreme.

---
+++ 4/4.7/extreme.py
@@ -0,0 +1,17 @@
+class CaseHelper:
+    
+    @staticmethod
+    def is_snake():
+        pass
+
+    @staticmethod
+    def is_upper_camel():
+        pass
+
+    @staticmethod
+    def to_snake():
+        pass
+
+    @staticmethod
+    def to_upper_camel():
+        pass
--- 4/4.7/strext.py
+++ 4/4.7/strext.py
@@ -10,7 +10,7 @@ class StrExtension:
 
     @staticmethod
     def leave_alpha(string):
-        for char in re.findall(r'\W|\d', string, flags=re.IGNORECASE):
+        for char in re.findall(r'[^A-Za-z]', string, flags=re.IGNORECASE):
             string = string.replace(char, '')
         return string
 
@@ -21,5 +21,34 @@ class StrExtension:
         return string
 
 
+# INPUT DATA:
+
+# TEST_1:
+print(StrExtension.remove_vowels('Python'))
+print(StrExtension.remove_vowels('Stepik'))
+
+# TEST_2:
+print(StrExtension.leave_alpha('Python111'))
+print(StrExtension.leave_alpha('__Stepik__()'))
+
+# TEST_3:
 print(StrExtension.replace_all('Python', 'Ptn', '-'))
 print(StrExtension.replace_all('Stepik', 'stk', '#'))
+
+# TEST_4:
+print(StrExtension.remove_vowels('Success is the ability to go from failure to failure without losing your enthusiasm.'))
+print(StrExtension.remove_vowels('Success is the ability to go from failure to failure without losing your enthusiasm.'.upper()))
+
+# TEST_5:
+print(StrExtension.leave_alpha('beegeek!\"#$%&\'()*+, -./:;<=>?@[\]^_`{|}~BEEGEEK'))
+print(StrExtension.leave_alpha('beegeek0123456789BEEGEEK'))
+
+# TEST_6:
+from string import ascii_lowercase
+
+text = '''I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches mathematics, and my mother is a nurse at a big hospital. My brothers are very smart and work hard in school. My sister is a nervous girl, but she is very kind. My grandmother also lives with us. She came from Italy when I was two years old. She has grown old, but she is still very strong. She cooks the best food!
+My family is very important to me. We do lots of things together. My brothers and I like to go on long walks in the mountains. My sister likes to cook with my grandmother. On the weekends we all play board games together. We laugh and always have a good time. I love my family very much.'''
+
+print(StrExtension.remove_vowels(text))
+print(StrExtension.leave_alpha(text))
+print(StrExtension.replace_all(text, ascii_lowercase, ''))
